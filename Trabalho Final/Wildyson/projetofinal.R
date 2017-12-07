5aa90cd91fca7f4c4f436297f1c61c10257cde7c
Wildyson Dantas <wildyson.santos@gmail.com>
2017-12-07 17:43:11 -0200

    Update projetofinal.R

--- Trabalho Final/Wildyson/projetofinal.R
+++ Trabalho Final/Wildyson/projetofinal.R
@@ -1,32 +1,33 @@
 library(caret)
 library(ggplot2)
 require(dplyr)
-
+library(rpart)
 
 biop <- read.csv("C:/Users/wildyson/UFPI/6 periodo/Sistemas Inteligentes/biopsy.csv", sep = ",")
 biop <- na.omit(biop)
+#biop <- biop[,-1]
 
 dim(biop)
 names(biop)
 
 
 #Treinamento
-intrain = createDataPartition(y = biop$V3, p=0.7, list = FALSE)
+intrain = createDataPartition(y = biop$class, p=0.7, list = FALSE)
 training <- biop[intrain,]
 testing <- biop[-intrain,]
 dim(training); dim(testing)
 
 #cross validation
 fitControl <- trainControl(method="cv", number = 10)
-lm <- lm(V2 ~ V3 + V8 + V9 + V1  + V4 + V5 + V6 + V7, data = biop)
-summary(lm)
+modelo <- train(class ~., method="rpart", data = training, trControl = fitControl)
 
-#Previsao x obeservao
-prediction <- predict(lm, select(testing,V1,V2,V3,V4,V5,V6,V7,V8,V9))
-lm_prediction <- data.frame(pred = prediction, obs = testing$V3)
+#Grafico
+prediction <- predict(modelo, testing)
+lm_prediction <- data.frame(pred = prediction, obs = testing$class)
 ggplot(lm_prediction, aes(x=pred, y = obs))+
   geom_point(alpha = 0.5, position = position_jitter(width = 0.2)) + 
-  geom_abline(colour = "blue") + ggtitle("Previsao x Observado (validação)")
+  geom_abline(colour = "blue") + ggtitle("Previsao x Observado (validaÃ§Ã£o)")
 
 #RMSE
+
 round(defaultSummary(lm_prediction), digits = 3)
